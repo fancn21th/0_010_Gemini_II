e8baaaf2dfdcf742165ef3a3ef132b1ab1d32aac
fancn <fanyj@yikeer.com>
2017-03-31 11:17:31 +0800

    third commit - hello world

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './index.css'
+
+render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: debug ? "inline-sourcemap" : null,
 	entry: "./src/index.js",
 	module: {
-	loaders: [
+		loaders: [
 		{
 			test: /\.jsx?$/,
 			exclude: /(node_modules|bower_components)/,
 			loader: 'babel-loader',
 			query: {
-				presets: ['react', 'es2015', 'stage-0'],
+				presets: ['react', 'es2015', 'stage-1'],
 				plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
 			}
 		},
